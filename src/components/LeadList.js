059904cbda2d9f15dc3ddd6d45ca30aaea2eee84
Riccardo Carzaniga <riccardo.carzaniga@canstar.com.au>
2023-05-23 05:09:12 +1000

    Lezione 9

--- src/components/LeadList.js
+++ src/components/LeadList.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  Box,
+  Button,
   Divider,
   Grid,
   IconButton,
   List,
   ListItem,
+  Modal,
+  Typography,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditNoteIcon from "@mui/icons-material/EditNote";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 const LeadList = (payload) => {
-  const handleDeleteButton = (leadId) => {
-    payload.deleteLead(leadId);
+  const [modal, setModal] = useState({
+    title: "",
+    message: "",
+    show: false,
+  });
+
+  const [currentLead, setCurrentLead] = useState(null);
+
+  const handleModal = (lead) => {
+    setCurrentLead(lead);
+    setModal({
+      show: true,
+      title: "CANCELLAZIONE",
+      message: "Sei sicuro di voler continuare con la cancellazione?",
+    });
+  };
+
+  const handleDeleteButton = () => {
+    payload.deleteLead(currentLead.leadId);
+    setModal({
+      ...modal,
+      show: false,
+    });
   };
 
   return (
@@ -31,7 +68,7 @@ const LeadList = (payload) => {
                   <IconButton
                     aria-label="delete"
                     color="primary"
-                    onClick={() => handleDeleteButton(entry.leadId)}
+                    onClick={() => handleModal(entry)}
                     sx={{
                       marginRight: "10px",
                     }}
@@ -50,6 +87,46 @@ const LeadList = (payload) => {
               </Grid>
             </ListItem>
             <Divider />
+
+            <Modal
+              open={modal.show}
+              onClose={() => {
+                setModal({ ...modal, show: false });
+              }}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  {modal.title}
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  {modal.message}
+                </Typography>
+                <Box
+                  sx={{
+                    marginTop: "30px",
+                    display: "flex",
+                    justifyContent: "space-around",
+                  }}
+                >
+                  <Button
+                    variant="outlined"
+                    onClick={() => {
+                      setModal({ ...modal, show: false });
+                    }}
+                  >
+                    Annulla
+                  </Button>
+                  <Button
+                    variant="contained"
+                    onClick={() => handleDeleteButton()}
+                  >
+                    Conferma
+                  </Button>
+                </Box>
+              </Box>
+            </Modal>
           </>
         );
       })}
